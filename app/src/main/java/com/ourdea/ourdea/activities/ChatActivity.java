e20a727f4d8eba66433d5eea132956fcd92bc0a2
DKSquad <dksquad@dksquad.com>
2015-03-05 09:43:07 -0500

    get projectId from session for chat

--- app/src/main/java/com/ourdea/ourdea/activities/ChatActivity.java
+++ app/src/main/java/com/ourdea/ourdea/activities/ChatActivity.java
@@ -1,13 +1,13 @@
 package com.ourdea.ourdea.activities;
 
 import android.content.Context;
-import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 
 import com.ourdea.ourdea.R;
 import com.ourdea.ourdea.fragments.ChatFragment;
+import com.ourdea.ourdea.resources.ApiUtilities;
 
 public class ChatActivity extends FragmentActivity {
 
@@ -21,12 +21,10 @@ public class ChatActivity extends FragmentActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
 
-        Intent givenIntent = getIntent();
-        mProjectId = givenIntent.getIntExtra(getString(R.string.PROPERTY_PROJECT_ID), 0);
+        mProjectId = ApiUtilities.Session.getProjectId(getApplicationContext()).intValue();
         SharedPreferences prefs = getSharedPreferences(getString(R.string.PROPERTY_NAME), Context.MODE_PRIVATE);
         mEmail = prefs.getString(getString(R.string.PROPERTY_EMAIL), "");
         mName = prefs.getString(getString(R.string.PROPERTY_USER_NAME), "");
-        mProjectId = 1;
         fragment_chat = (ChatFragment) getSupportFragmentManager().findFragmentById(R.id.fragment_chat);
         fragment_chat.initializeChat(mProjectId, mEmail, mName);
     }
